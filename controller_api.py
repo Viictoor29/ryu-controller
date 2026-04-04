eacce7e260069b45fddd651ddb0509bf4fd139a0
Victor Pinilla <vicpincor29@gmail.com>
2026-04-04 15:45:15 +0000

    add: implement SDNControllerAPI and SDNRestController with topology API endpoint; add .gitignore and simple_topo.py

--- controller_api.py
+++ controller_api.py
@@ -5,9 +5,72 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.app.wsgi import WSGIApplication, ControllerBase, route
 from webob import Response
 
-from ryu.topology.api import get_switch, get_link
+from ryu.topology.api import get_switch, get_link, get_host
 import json
 import time
 import traceback
 
 
+API_INSTANCE_NAME = "sdn_api_app"
+
+class SDNControllerAPI(app_manager.RyuApp):
+    _CONTEXTS = {
+        "wsgi": WSGIApplication
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(SDNControllerAPI, self).__init__(*args, **kwargs)
+
+        wsgi = kwargs["wsgi"]
+        wsgi.register(SDNRestController, {
+            API_INSTANCE_NAME: self
+        })
+
+class SDNRestController(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(SDNRestController, self).__init__(req, link, data, **config)
+        self.sdn_app = data[API_INSTANCE_NAME]
+
+    @route("topology", "/api/topology", methods=["GET"])
+    def get_topology(self, req, **kwargs):
+        switches = get_switch(self.sdn_app, None)
+        links = get_link(self.sdn_app, None)
+        hosts = get_host(self.sdn_app, None)
+
+        nodes = []
+        edges = []
+
+        # switches → nodos
+        for sw in switches:
+            nodes.append({
+                "id": sw.dp.id,
+                "type": "switch"
+            })
+
+        # hosts → nodos
+        for host in hosts:
+            nodes.append({
+                "id": host.mac,
+                "type": "host",
+                "connected_to": host.port.dpid
+            })
+
+        # links → aristas
+        for link in links:
+            edges.append({
+                "source": link.src.dpid,
+                "target": link.dst.dpid,
+                "src_port": link.src.port_no,
+                "dst_port": link.dst.port_no
+            })
+
+        body = {
+            "nodes": nodes,
+            "edges": edges
+        }
+
+        return Response(
+            content_type="application/json",
+            charset="utf-8",
+            body=json.dumps(body).encode("utf-8")
+        )
---
+++ simple_topo.py
@@ -0,0 +1,17 @@
+from mininet.topo import Topo
+
+
+class SimpleTopo(Topo):
+    def build(self):
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+
+        self.addLink(h1, s1)
+        self.addLink(s1, s2)
+        self.addLink(s2, h2)
+
+
+topos = {'simpletopo': lambda: SimpleTopo()}
