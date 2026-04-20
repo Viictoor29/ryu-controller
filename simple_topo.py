747266aa6ee19dba9542ac49bbc527a4dd6f18e5
Victor Pinilla <vicpincor29@gmail.com>
2026-04-20 22:34:47 +0000

    CHECK

--- simple_topo.py
+++ simple_topo.py
@@ -7,7 +7,7 @@ class SimpleTopo(Topo):
         s2 = self.addSwitch('s2')
 
         h1 = self.addHost('h2', ip='10.0.0.1/24')
-        h2 = self.addHost('h1', ip='10.0.0.2/24')
+        h2 = self.addHost('h1', ip='10.0.0.69/24')
 
         self.addLink(h1, s1)
         self.addLink(s1, s2)
